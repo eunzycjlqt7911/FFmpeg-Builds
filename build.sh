138accb42cfe3b21ecf7730633eef4e9fc8fa3f3
kukk <kukk@q.com>
2025-06-08 10:10:13 +0800

    script

--- build.sh
+++ build.sh
@@ -34,6 +34,10 @@ cat <<EOF >"$BUILD_SCRIPT"
 
     git clone --filter=blob:none --branch='$GIT_BRANCH' '$FFMPEG_REPO' ffmpeg
     cd ffmpeg
+    
+    # Add execute permissions
+    chmod +x configure
+    chmod +x ffbuild/version.sh
 
     ./configure --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS \$FF_CONFIGURE \
         --extra-cflags="\$FF_CFLAGS" --extra-cxxflags="\$FF_CXXFLAGS" --extra-libs="\$FF_LIBS" \
